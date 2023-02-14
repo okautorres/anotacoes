72c412ef1b4e0496e4cf12199fdd651318954296
Howlter <darqueboost@gmail.com>
2023-02-13 22:23:40 -0300

    Finalizando design e animações, (+) e de saída, com JS, HTML e CSS.

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
-let novo = document.querySelector('control um ');
 
-novo.addEventListener('click', ()=>{
-    
+document.querySelector('.menu-opener p').addEventListener('click', ()=>{
+    setTimeout(() => {
+        document.querySelector('.menu-opener').style.marginLeft = '-300';
+        document.querySelector('.menu').style.marginLeft = "0";
+    }, 100);
+    document.querySelector('.menu').style.display = 'flex';
+
+});
+
+document.querySelector('.control.exit').addEventListener('click', ()=>{
+    setTimeout(() => {
+        document.querySelector('.menu').style.marginLeft = '-300';
+        document.querySelector('.menu-opener').style.marginLeft = "0";
+    }, 100);
+    document.querySelector('.menu-opener').style.display = 'flex';
+
+});
+
+document.querySelector('.adicionar input, .adicionar textarea').addEventListener('click', ()=>{
+    setTimeout(() => {
+        document.querySelector('.adicionar').style.marginLeft = '15%';
+        document.querySelector('.adicionar').style.height = '70%';
+        document.querySelector('.adicionar').style.width = '70%';
+    }, 100);
+    document.querySelector('body').addEventListener('click', ()=>{
+        document.querySelector('.adicionar').style.marginLeft = '35%';
+        document.querySelector('.adicionar').style.height = '30%';
+        document.querySelector('.adicionar').style.width = '30%';
+    });
+});
+
+document.querySelector('.adicionar textarea').addEventListener('click', ()=>{
+    setTimeout(() => {
+        document.querySelector('.adicionar').style.marginLeft = '15%';
+        document.querySelector('.adicionar').style.height = '70%';
+        document.querySelector('.adicionar').style.width = '70%';
+    }, 100);
+    document.querySelector('body').addEventListener('click', ()=>{
+        document.querySelector('.adicionar').style.marginLeft = '35%';
+        document.querySelector('.adicionar').style.height = '30%';
+        document.querySelector('.adicionar').style.width = '30%';
+    });
+});
+
+document.querySelector('.control-anotation').addEventListener('click', ()=>{
+    document.querySelector('.adicionar').style.display = 'none';
+    document.querySelector('.adicionar-mae').style.display = 'none';
+    setTimeout(() => {
+        document.querySelector('.adicionar').style.height = '10%';
+        document.querySelector('.adicionar').style.width = '10%';
+    }, 100);
+});
+
+document.querySelector('.control.um').addEventListener('click', ()=>{
+    document.querySelector('.adicionar').style.display = 'flex';
+    document.querySelector('.adicionar-mae').style.display = 'flex';
+    setTimeout(() => {
+    document.querySelector('.adicionar').style.height = '30%';
+    document.querySelector('.adicionar').style.width = '30%';
+}, 100);
 });
